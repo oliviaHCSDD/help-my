3844798a8990d1f4c39a9c6c4bb1340e1da6f370
oliviaHCSDD <olivia.2.7363920@gmail.com>
2026-01-06 04:41:05 -0800

    Add login form and authentication handling
    Implement login functionality with Firebase authentication.

---
+++ admin.js
@@ -0,0 +1,27 @@
+import { auth } from "./app.js";
+import { onAuthStateChanged, signInWithEmailAndPassword } 
+from "https://www.gstatic.com/firebasejs/10.12.5/firebase-auth.js";
+
+const loginForm = document.getElementById("loginForm");
+const errorBox = document.getElementById("error");
+
+loginForm.addEventListener("submit", async (e) => {
+  e.preventDefault();
+
+  const email = loginForm.email.value;
+  const password = loginForm.password.value;
+
+  try {
+    await signInWithEmailAndPassword(auth, email, password);
+    window.location.href = "./dashboard.html";
+  } catch (err) {
+    errorBox.textContent = err.message;
+  }
+});
+
+// حماية الصفحة
+onAuthStateChanged(auth, (user) => {
+  if (user) {
+    console.log("Logged in:", user.email);
+  }
+});
